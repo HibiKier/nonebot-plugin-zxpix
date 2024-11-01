d25c4019f4cfb58579884b5cc17bc7203712eb5b
HibiKier <775757368@qq.com>
2024-11-01 11:00:42 +0800

    🎨 导入优化

--- nonebot_plugin_zxpix/__init__.py
+++ nonebot_plugin_zxpix/__init__.py
@@ -53,4 +53,5 @@ from .pix import *  # noqa: F403
 from .pix_info import *  # noqa: F403
 from .pix_keyword import *  # noqa: F403
 from .pix_set import *  # noqa: F403
+from .pix_star import *  # noqa: F403
 from .token import *  # noqa: F403
